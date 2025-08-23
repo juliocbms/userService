a6d909479ed0986f4338683e7421e9ddfda73443
Julio <juliocesar13b@gmail.com>
2025-08-22 21:44:34 -0300

    ajustando configurações do spring security

---
+++ src/main/java/com/microservice/User/Config/Security/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.microservice.User.Config.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws  Exception{
+        return httpSecurity.
+                csrf( c -> c.disable())
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(a -> a.anyRequest().authenticated())
+                .build();
+    }
+}
--- src/main/java/com/microservice/User/Repository/UserRepository.java
+++ src/main/java/com/microservice/User/Repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.microservice.User.Repository;
 
 import com.microservice.User.Models.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    UserDetails findByEmail(String email);
 }
